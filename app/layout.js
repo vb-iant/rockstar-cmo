ce1e4b1c18430fb3b613a232a4e03ac6c34b0e2f
vb-iant <ian@velocity-b.com>
2026-08-28 18:57:54 +0100

    Add Google Tag Manager (GTM-N6HHRZRR) -- same container ID as the old WordPress site

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,10 @@
+import Script from "next/script";
 import { Bevan, Domine, Special_Elite } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+
+const GTM_ID = "GTM-N6HHRZRR";
 
 const bevan = Bevan({
   subsets: ["latin"],
@@ -35,7 +38,26 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${bevan.variable} ${domine.variable} ${specialElite.variable}`}>
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`}
+        </Script>
+      </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Header />
         {children}
         <Footer />
